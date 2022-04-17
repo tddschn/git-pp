79e9da94a5c6916f336016a6b59624daa9236998
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-04-17 20:24:08 +0800

    make logging-utils-tddschn a dev dep

--- git_pp/__init__.py
+++ git_pp/__init__.py
@@ -5,13 +5,7 @@ try:
 	from logging_utils_tddschn import get_logger # type: ignore
 	logger, _ = get_logger(__app_name__)
 except:
-	# try:
-	#     from loguru import logger  # type: ignore
-	#     logger.disable(__name__)
-	# except:
 	import logging
 	from logging import NullHandler
-	# logging.basicConfig(level=logging.NOTSET)
 	logger = logging.getLogger(__name__)
 	logger.addHandler(NullHandler())
-	logger.setLevel(logging.INFO)
