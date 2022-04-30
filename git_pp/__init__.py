4bc70f3a1346b59ca67fde4085ec4ccd23d743c1
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-04-30 09:16:25 +0800

    yapf

--- git_pp/__init__.py
+++ git_pp/__init__.py
@@ -2,10 +2,10 @@ __version__ = '1.10.0'
 __app_name__ = 'git pp'
 
 try:
-	from logging_utils_tddschn import get_logger # type: ignore
-	logger, _ = get_logger(__app_name__)
+    from logging_utils_tddschn import get_logger  # type: ignore
+    logger, _ = get_logger(__app_name__)
 except:
-	import logging
-	from logging import NullHandler
-	logger = logging.getLogger(__name__)
-	logger.addHandler(NullHandler())
+    import logging
+    from logging import NullHandler
+    logger = logging.getLogger(__name__)
+    logger.addHandler(NullHandler())
--- git_pp/git_pp.py
+++ git_pp/git_pp.py
@@ -136,5 +136,6 @@ async def main():
 def main_sync():
     asyncio.run(main())
 
+
 if __name__ == '__main__':
     main_sync()
