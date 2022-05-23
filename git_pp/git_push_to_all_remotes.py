4e1b25ffadc088a40ac7bf96f1e80d1601c026a4
Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-05-23 10:18:55 +0800

    move util funcs to another module

--- git_pp/git_push_to_all_remotes.py
+++ git_pp/git_push_to_all_remotes.py
@@ -3,27 +3,10 @@
 import asyncio, sys
 from asyncio.subprocess import Process
 from os import PathLike
+from utils_tddschn.git import git_get_current_branch, git_get_all_remotes
 
 from . import logger
 
-
-async def git_get_all_remotes(cwd: PathLike | None = None) -> list[str]:
-    program = ['git', 'remote']
-    proc: Process = await asyncio.create_subprocess_exec(
-        *program, stdout=asyncio.subprocess.PIPE, cwd=cwd)
-    logger.info(f'Getting all remotes of {cwd} .')
-    logger.info(f'Running: {program}')
-    # logger.info(f'Args: {proc.__dict__}')  # type: ignore
-    stdout, _ = await proc.communicate()
-    return stdout.decode().strip().splitlines()
-
-
-async def git_get_current_branch(cwd: PathLike | None = None) -> str:
-    program = ['git', 'branch', '--show-current']
-    proc: Process = await asyncio.create_subprocess_exec(
-        *program, stdout=asyncio.subprocess.PIPE, cwd=cwd)
-    stdout, _ = await proc.communicate()
-    return stdout.decode().strip()
 
 
 
